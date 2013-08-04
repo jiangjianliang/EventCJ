167c4178cd22e5018a142422ae70e3307424cda9
蒋建亮 <jianliang.j@gmail.com>
2013-08-04 14:25:53 +0800

    libeventcj version 1.1
    1. 在layer中增加了一个MAP,未测试

--- libeventcj/src/eventcj/Layer.java
+++ libeventcj/src/eventcj/Layer.java
@@ -1,11 +1,22 @@
 package eventcj;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Layer {
+	private Map<String, Object> map = new HashMap<String, Object>();
 	public abstract void activate();
 	public abstract void deactivate();
 	//featured by wander
 	public abstract void pause();
 	public abstract void restore();
+	public void put(String name, Object obj){
+		map.put(name, obj);
+	}
+	public Object get(String name){
+		Object obj = map.get(name);
+		return obj;
+	}
 	//end
 	public abstract String getName();
 	@Override
